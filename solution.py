8f83435fbc75ad264dfaa0a8c8682608e8d345dc
Rasulchek <rasulglash@gmail.com>
2023-04-07 23:38:04 +0300

    я не лох

--- solution.py
+++ solution.py
@@ -2,12 +2,15 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import chi2
+from scipy.stats import chi2
 from scipy.stats import norm
 
 chat_id = 680977959  # Ваш chat ID, не меняйте название переменной
 
 
 def solution(p: float, x: np.array) -> tuple:
+    from scipy.stats import chi2
+
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
