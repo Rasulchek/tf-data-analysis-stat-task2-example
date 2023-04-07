d900388d7674c479e6c1a329ea34883893c13f9a
Rasulchek <rasulglash@gmail.com>
2023-04-07 22:58:14 +0300

    я лох

--- solution.py
+++ solution.py
@@ -10,22 +10,22 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-   squared_distances = x**2
- 
+    squared_distances = x**2
+
     sum_squared_distances = np.sum(squared_distances)
- 
+
     n = len(x)
     df = n
 
     alpha = 1 - p
     chi2_low = chi2.ppf(alpha/2, df)
     chi2_high = chi2.ppf(1 - alpha/2, df)
- 
+
 
     sigma2_low = sum_squared_distances / chi2_high
     sigma2_high = sum_squared_distances / chi2_low
- 
+
     sigma_low = np.sqrt(sigma2_low / 50)
     sigma_high = np.sqrt(sigma2_high / 50)
- 
+
     return sigma_low, sigma_high
